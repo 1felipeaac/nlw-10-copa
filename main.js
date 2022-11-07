0728c7ae063c5bf12da96cfc5b7c935771d329b9
Felipe Coelho <felipeaacoelho@gmail.com>
2022-11-07 12:31:10 -0300

    change functions

--- main.js
+++ main.js
@@ -1,72 +1,95 @@
+// const arrayGame = []
 function createGame(player1, hour, player2, group) {
-  return `
+  // arrayGame.push([player1, hour, player2, group])
+  return (
+    `
   <li>
       <div class="info-country">
         <img class="country" src="./assets/icon-${player1}.svg" alt=""><span class="name-country">${player1}</span>
       </div>
       <div id="details">
         <strong>Grupo ${group}</strong>
-        <strong>${hour}</strong>
+        <strong>${hour}:00</strong>
       </div>
       <div class="info-country">
         <img class="country" src="./assets/icon-${player2}.svg" alt=""><span class="name-country">${player2}</span>
       </div>
   </li>
   `
+  )
 }
 
 let delay = -0.4
-function createCard(date, day, games) {
+// const arrayDate = [];
+function createCard(date, games) {
+  game_day = new Date(date)
+  var week = [
+    "Domingo",
+    "Segunda",
+    "Terça",
+    "Quarta",
+    "Quinta",
+    "Sexta",
+    "Sábado",
+  ]
   delay = delay + 0.4
-  return `
+  // arrayDate.push([game_day.getDate() + "/" + (game_day.getMonth() + 1)])
+  return (
+    `
   <div class="card" style="animation-delay: ${delay}s">
-      <h2>${date} <span>${day}</span></h2>
+      <h2>${game_day.getDate() + "/" + (game_day.getMonth() + 1)} <span>${
+      week[game_day.getDay()]
+    }</span></h2>
       <ul>
         ${games}
       </ul>
   </div>
-          `
+  `
+  )
 }
 
 document.querySelector("#cards").innerHTML =
   createCard(
-    "24/11",
-    "quinta",
-    createGame("switzerland", "07:00", "cameroon", "g") +
-      createGame("uruguay", "10:00", "south-korea", "h") +
-      createGame("ghana", "13:00", "portugal", "h") +
-      createGame("brazil", "16:00", "serbia", "g")
+    "11/24/2022",
+    createGame("switzerland", "07", "cameroon", "g") +
+      createGame("uruguay", "10", "south-korea", "h") +
+      createGame("ghana", "13", "portugal", "h") +
+      createGame("brazil", "16", "serbia", "g")
   ) +
   createCard(
-    "28/11",
-    "segunda",
-    createGame("cameroon", "07:00", "serbia", "g") +
-      createGame("south-korea", "10:00", "ghana", "h") +
-      createGame("brazil", "13:00", "switzerland", "g") +
-      createGame("portugal", "16:00", "uruguay", "h")
+    "11/28/2022",
+    createGame("cameroon", "07", "serbia", "g") +
+      createGame("south-korea", "10", "ghana", "h") +
+      createGame("brazil", "13", "switzerland", "g") +
+      createGame("portugal", "16", "uruguay", "h")
   ) +
   createCard(
-    "02/12",
-    "sexta",
-    createGame("south-korea", "12:00", "portugal", "h") +
-      createGame("ghana", "12:00", "uruguay", "h") +
-      createGame("serbia", "13:00", "switzerland", "g") +
-      createGame("cameroon", "16:00", "brazil", "g")
+    "12/02/2022",
+    createGame("south-korea", "12", "portugal", "h") +
+      createGame("ghana", "12", "uruguay", "h") +
+      createGame("serbia", "13", "switzerland", "g") +
+      createGame("cameroon", "16", "brazil", "g")
   )
 
+// const arrayCards = []
+
+// arrayCards.push(arrayDate, arrayGame)
+
+// console.log(arrayCards)
+
 //events
-const flag_country = document.getElementsByClassName("country");
-const name_country = document.getElementsByClassName("name-country"); 
+const flag_country = document.getElementsByClassName("country")
+const name_country = document.getElementsByClassName("name-country")
 
 for (let index = 0; index < flag_country.length; index++) {
-  const this_flag = flag_country[index];
-  const this_country = name_country[index];
+  const this_flag = flag_country[index]
+  const this_country = name_country[index]
 
   this_flag.addEventListener("mouseover", event_mouseouver)
 
   this_flag.addEventListener("mouseout", event_mouseout)
 
-  function event_mouseouver(){
+  function event_mouseouver() {
     this_country.style.display = "block"
     this_flag.style.transform = "scale(0.7)"
   }
@@ -75,5 +98,4 @@ for (let index = 0; index < flag_country.length; index++) {
     this_country.style.display = "none"
     this_flag.style.transform = "scale(1)"
   }
-
 }
